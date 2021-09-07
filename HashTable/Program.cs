1af71c922ab9a51b67812f0ef1b7a2d365d27846
darshan magare <darshan.magare1234@gmail.com>
2021-09-07 09:40:09 +0530

    Add project files.

---
+++ HashTable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Program obj2=new Program();
+            obj2.HashTableMethod();
+            Console.ReadKey();
+        }
+    }
+
+    class Hashtable1
+    {
+        public void HashTableMethod();
+        {
+            HashT obj = new HashT();
+            obj.Add("First","C#");
+            obj.Add("Second",3);
+            obj.Add("Third","C++");
+            obj.Add("Fourth","Two");
+            obj.Add("Fifth","One");
+
+
+            Console.WriteLine("Keys and Vaules :");
+            Printkeyandvalues(obj);
+
+            Console.WriteLine("Value for key 'Second ' is  "+ obj["Second"]);
+
+        }
+
+    public void Printkeyandvalues(HashTable1 myList)
+     {
+        HashTable myEnnumnerator = mylist HashTable();
+        while (myEnnumnerator.MoveNext())
+        {
+            Console.WriteLine("\t{0}:\t{1}", myEnnumnerator.Key, myEnnumnerator.Value);
+        }
+        Console.WriteLine();
+      }
+
+    
+    }
+}
